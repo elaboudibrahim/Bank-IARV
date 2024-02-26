84b19350c8ee9023d1603a75f439d5dee0c81ebf
Mouataz billah <mouatazfarri@gmail.com>
2024-02-26 11:33:12 +0100

    creer fct payee facture dans G_P

--- packages/Gestion_de_paiment.py
+++ packages/Gestion_de_paiment.py
@@ -15,3 +15,21 @@ def afficher_facture(ref):
             print(str(j+1) + ")-- type de facture :  " + facture[0] + " montant "  + facture[1] + "\n")
     except:
             print("fin-----------------------------------------------------------")
+
+
+
+def paiment_facture(ref,num):
+    compte=Assistant.trouverCompte(ref)
+    file = open("files/users/facture/" + str(compte) + ".txt", 'r')
+    data = file.readlines()
+    for i in range(len(data)):
+        if i+1 == num:
+            facture=data[i].split("**")
+            try:
+                Assistant.reduitSolde(compte, float(facture[1]))
+                data.pop(i)
+                file2 = open("files/users/facture/" + str(compte) + ".txt", 'w')
+                file2.writelines(data)
+                print("paiment succes  de la facture : ", facture[0] ," \n")
+            except:
+                print("vérifier votre solde \n")
