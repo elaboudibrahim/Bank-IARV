887f225bd5c13514446502a27e4c3bbd1cb793e4
Mouataz billah <mouatazfarri@gmail.com>
2024-02-26 11:26:28 +0100

    cree fct gestion paiment

---
+++ packages/Gestion_de_paiment.py
@@ -0,0 +1,18 @@
+from . import Assistant
+
+def afficher_facture(ref):
+
+    compte=Assistant.trouverCompte(ref)
+    file = open("files/users/facture/" + str(compte) + ".txt", 'r')
+    data = file.readlines()
+    print("debut----------------------------------------------------------")
+    print("vous avez ", len(data), " facture à regler\n")
+    print("----------------------------------------------------------")
+
+    try:
+        for i,j in zip(data, range(len(data))):
+            facture=i.split("**")
+            print(str(j+1) + ")-- type de facture :  " + facture[0] + " montant "  + facture[1] + "\n")
+    except:
+            print("fin----------------------------------------------------------")
+
